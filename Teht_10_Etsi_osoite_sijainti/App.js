178ac697541ff162a2dd48061dd94fd94b101359
Sami Hiltunen <sami@Mac.localdomain>
2026-02-19 21:44:06 +0200

    Tehtävä 10 valmis

---
+++ Teht_10_Etsi_osoite_sijainti/App.js
@@ -0,0 +1,118 @@
+//Testattu ainoastaan iOS-simulaattorilla!
+//https://haagahelia.github.io/mobilecourse/
+//https://geocode.maps.co/docs/
+import {useState, useEffect } from 'react';
+import {Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import * as Location from 'expo-location';
+
+const EtsiOsoiteSijainti = () => {
+
+const GEOCODE_API_KEY = process.env.EXPO_PUBLIC_GEOCODE_API_KEY;
+
+const [address, setAddress] = useState(null);
+const [region, setRegion] = useState({ 
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221,
+});
+const [marker, setMarker] = useState(null);
+const [latitude, setLatitude] = useState(null); 
+const [longitude, setLongitude] = useState(null);
+
+useEffect(() => {
+  (async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('No permission to get location')
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+
+    const { latitude, longitude } = location.coords;
+   
+    setLatitude(latitude); 
+    setLongitude(longitude);  
+
+    setRegion({ ...region, 
+      latitude, 
+      longitude, }); 
+
+    setMarker({ latitude, longitude }); 
+
+  })();
+}, []);
+
+
+const searchAddress = () => { 
+
+  const url = `https://geocode.maps.co/search?q=${address}&api_key=${GEOCODE_API_KEY}`;
+   
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //Jos osoitetta ei löydy
+      if (data.length === 0) { 
+       console.log("Address not found");
+       return; 
+      }
+
+      //määritä lat & lon koordinaatit muuttujiin
+      const { lat, lon } = data[0]; 
+      const latitude = parseFloat(lat); 
+      const longitude = parseFloat(lon);  
+
+      setRegion({ ...region, 
+          latitude, 
+          longitude, }); 
+  
+      setMarker({ latitude, longitude }); 
+
+    })
+    .catch(error => console.log('error', error));    
+};
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>    
+      <MapView
+        style={{ width: '100%', height: '90%' }} region={region}>
+          {marker && <Marker coordinate={marker} />}
+      </MapView>      
+      <TextInput
+            style={styles.input}
+            onChangeText={setAddress}
+            value={address}
+      />
+      <TouchableOpacity style={styles.button}> 
+        <Button color="white" title="Show" onPress={searchAddress}/>      
+      </TouchableOpacity>    
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center'
+  },
+  input: { //input
+    height: 40,
+    width: 300,
+    padding: 5,
+    borderWidth: 1,
+    margin: 5,
+    alignItems: 'center',
+    justifyContent: 'center'  
+  },
+  button: { //painike
+    backgroundColor: "#008ef3", 
+    margin: 10,
+    borderRadius: 5
+    },
+});
+
+
+export default EtsiOsoiteSijainti;
