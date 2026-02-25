bc914e2b393204790337932a09ffd19f2e93f543
Sami Hiltunen <sami@Mac.localdomain>
2026-02-25 17:55:43 +0200

    Tehtävä 8 täydennetty

--- Teht_8_Etsi_osoite/App.js
+++ Teht_8_Etsi_osoite/App.js
@@ -2,7 +2,7 @@
 //https://haagahelia.github.io/mobilecourse/
 //https://geocode.maps.co/docs/
 import {useState } from 'react';
-import {Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import {Button, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
@@ -48,6 +48,7 @@ const searchAddress = () => {
 };
 
   return (
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>    
       <MapView
@@ -64,6 +65,7 @@ const searchAddress = () => {
       </TouchableOpacity>    
       </SafeAreaView>
     </SafeAreaProvider>
+    </KeyboardAvoidingView>
   );
 };
 
